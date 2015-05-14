daf4764a5c3f142afe7363080a35429fb939baf2
Carlos Cordoba <ccordoba12@gmail.com>
2015-05-14 14:50:59 -0500

    Finish symlink script

--- symlink-qt.py
+++ symlink-qt.py
@@ -3,18 +3,37 @@
 Symlink External Qt in Homebrew
 """
 
+from __future__ import print_function
+
 import os
 import os.path as osp
 import subprocess
 
-QT_VERSION = "4.9.9"
+# Getting the directory where we're going to symlink Qt
+QT_VERSION = "4.8.6"
 CELLAR = 'HOMEBREW_CELLAR'
 
 brew_config = subprocess.check_output(['brew', '--config']).split('\n')
 cellar_config = [c for c in brew_config if c.startswith(CELLAR)][0]
 cellar_dir = cellar_config.split()[1]
+brew_qt_dir = osp.join(cellar_dir, 'qt', QT_VERSION)
 
+# Creating the Homebrew Qt dir
 try:
-    os.makedirs(osp.join(cellar_dir, QT_VERSION, 'Frameworks'))
+    os.makedirs(brew_qt_dir)
+    os.chdir(brew_qt_dir)
 except:
-    pass
+    print("Error creating Qt dir under Homebrew")
+
+
+# Symlink the external Qt installation under the previous directory
+try:
+    os.symlink('/Developer/Tools/Qt', './bin')
+    os.symlink('/Library/Frameworks', './Frameworks')
+    os.symlink('/Developer/Applications/Qt/imports', './imports')
+    os.symlink('/usr/local/Qt4.8/mkspecs', './mkspecs')
+    os.symlink('/Developer/Applications/Qt/phrasebooks', './phrasebooks')
+    os.symlink('/Developer/Applications/Qt/plugins/', './plugins')
+    os.symlink('/Developer/Applications/Qt/translations', './translations')
+except:
+    print("Error creating symlinks")
